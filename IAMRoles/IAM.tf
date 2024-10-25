204ee939c0cf97d62636cbca23a67137ac20555b
Trevor Trac <TrevorTrac@gmail.com>
2024-10-25 09:08:31 -0700

    m

--- IAMRoles/IAM.tf
+++ IAMRoles/IAM.tf
@@ -1,53 +1,3 @@
-/*
-resource "aws_iam_role" "test_role" {
-  name = var.role_name
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
-
-resource "aws_iam_policy" "test_policy" {
-  name        = "test_policy"
-  role = aws_iam_role.test_role.id
-  
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-*/
-
-
-
-
 resource "aws_iam_role_policy" "test_policy" {
   name = "test_policy"
   role = aws_iam_role.test_role.id
@@ -69,7 +19,7 @@ resource "aws_iam_role_policy" "test_policy" {
 }
 
 resource "aws_iam_role" "test_role" {
-  name = "test_role"
+  name = ${var.role_name} #"test_role" #dev_test_role
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
--- IAMRoles/dev.tfvars
+++ IAMRoles/dev.tfvars
@@ -1,1 +1,12 @@
 role_name = "dev_testing_role"
+
+/*
+
+But how can I make sure that my main is connected to these variable files in such a way that when i go into the dev directory i can do a terraform apply command and the infrastructure gets created in the dev enviroment same thing with production. I would really appreciate some more insight into this problem
+
+Before this I tried to work with terraform workspaces however it uses the same backand for all enviroments which reduced the flexibility i needed`
+
+*/
+
+
+
